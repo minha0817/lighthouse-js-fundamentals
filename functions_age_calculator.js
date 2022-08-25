d31093f7d585cc2c4c890ba6755b37da32aaf11a
minha0817 <minhakk24@gmail.com>
2022-08-24 21:57:50 -0700

    making ag e calculator with using function

---
+++ functions_age_calculator.js
@@ -0,0 +1,7 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  let age = currentYear - yearOfBirth;
+  let result = `${name} is ${age} years old.`;
+  return result;
+};
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
