6cfb5ab2996c4cb19b24a99e93229921c28ea136
minha0817 <minhakk24@gmail.com>
2022-08-25 16:39:59 -0700

    find last Index in Array

---
+++ lastIndexOf.js
@@ -0,0 +1,9 @@
+function lastIndexOf(arr, num){
+  return arr.lastIndexOf(num);
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([], 3), "=?", -1);
+
